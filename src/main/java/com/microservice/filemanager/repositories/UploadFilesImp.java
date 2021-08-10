b545d6afae8f6413e021d0db15a5a58c1a3c7192
panchuwu <francisco.nunez.d@usach.cl>
2021-08-10 00:52:08 -0400

    feat: create directory if doen't exists, and create a folder for every postulant

--- src/main/java/com/microservice/filemanager/repositories/UploadFilesImp.java
+++ src/main/java/com/microservice/filemanager/repositories/UploadFilesImp.java
@@ -4,7 +4,8 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.multipart.MultipartFile;
-
+// Java imports
+import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -14,28 +15,39 @@ import java.util.stream.Stream;
 @Repository
 public class UploadFilesImp implements UploadFiles{
 
-    private final Path rootFolder = Paths.get(System.getProperty("user.home")+"/uploaded-files");
+    private String PATH = "root/uploaded-files";
+//    private String PATH  = "/home/pancho/Desktop/uploaded-files";
+    private final File directory = new File( PATH );
 
     @Override
-    public void save(MultipartFile file) throws Exception{
-        Files.copy(file.getInputStream(), this.rootFolder.resolve(file.getOriginalFilename()));
+    public void save( MultipartFile file, String value ) throws Exception{
+        // if root directory doesn't exist, then create
+        if ( !directory.exists() ) directory.mkdir();
+        String newPath = PATH + "/" + value;
+        // same with the new directory route using postulant's name like value
+        File directoryRoute = new File ( newPath );
+        if ( !directoryRoute.exists() ) directoryRoute.mkdir();
+        Path rootFolder = Paths.get( newPath );
+        Files.copy(file.getInputStream(), rootFolder.resolve(file.getOriginalFilename()));
     }
 
     @Override
-    public Resource load(String name) throws Exception{
+    public Resource load(String name, String value) throws Exception{
+        Path rootFolder = Paths.get( PATH + value );
         Path file = rootFolder.resolve(name);
         return new UrlResource(file.toUri());
     }
 
     @Override
-    public void save(List<MultipartFile> files) throws Exception{
+    public void save(List<MultipartFile> files, String value) throws Exception{
         for (MultipartFile file : files) {
-            this.save(file);
+            this.save( file, value );
         }
     }
 
     @Override
     public Stream<Path> loadAll() throws Exception{
+        Path rootFolder = Paths.get( PATH );
         return Files.walk(rootFolder, 1).filter(path -> !path.equals(rootFolder)).map(rootFolder::relativize);
     }
 }
